9613daed4fb8bc64a7fd720e602e285efaaa3b53
Blake Williams <blake@blakewilliams.me>
2014-12-20 16:47:18 -0500

    Add faster version of fibs for Ruby and Rust
    * Ruby fibs will stackoverflow
    * Rust fibs can't go beyond 93 in Ruby due to FFI + ints

--- ext/fib/src/lib.rs
+++ ext/fib/src/lib.rs
@@ -1,11 +1,45 @@
-#[allow(dead_code)]
 #[no_mangle]
-pub extern "C" fn fib(x: uint) -> uint {
-    match x {
+pub extern "C" fn slow_fib(n: u64) -> u64 {
+    match n {
         0 => 0,
         1 => 1,
         n => {
-            fib(n - 1) + fib(n -2)
+            slow_fib(n - 1) + slow_fib(n -2)
         }
     }
+}
+
+#[no_mangle]
+pub extern "C" fn fib(n: u64) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        n => _fib(n, 1, 1)
+    }
+}
+
+pub fn _fib(target: u64, previous: u64, next: u64) -> u64 {
+    if target == 3 {
+        previous + next
+    } else {
+        _fib(target -1, next, previous + next)
+    }
+}
+
+#[test]
+fn fib_returns_0_when_passed_0() {
+    let result = fib(0);
+    assert_eq!(result, 0);
+}
+
+#[test]
+fn fib_returns_1_when_passed_1() {
+    let result = fib(1);
+    assert_eq!(result, 1);
+}
+
+#[test]
+fn fib_returns_13_when_passed_7() {
+    let result = fib(7);
+    assert_eq!(result, 13);
 }
--- fib.rb
+++ fib.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-#
+
 require "ffi"
 require "benchmark"
 
@@ -8,35 +8,53 @@ include Benchmark
 class Fib
   module Rust
     extend FFI::Library
-
     ffi_lib "./ext/fib/target/release/libfib-ef056b8c7f5021aa.dylib"
-    attach_function :fib, [:int], :int
+
+    attach_function :fib, [:int], :size_t
+    attach_function :slow_fib, [:int], :size_t
   end
 
 
-  def self.ruby_fib(n)
+  def self.slow_ruby_fib(n)
     if n <= 1
       n
     else
-      ruby_fib(n - 1) + ruby_fib(n - 2)
+      slow_ruby_fib(n - 1) + slow_ruby_fib(n - 2)
+    end
+  end
+
+  def self.ruby_fib(n, prev_fib = 1, next_fib = 1)
+    if n < 3
+      1
+    elsif n == 3
+      prev_fib + next_fib
+    else
+      ruby_fib(n - 1, next_fib, prev_fib + next_fib)
     end
   end
 
   def self.rust_fib(n)
     Fib::Rust::fib(n)
   end
+
+  def self.slow_rust_fib(n)
+    Fib::Rust::slow_fib(n)
+  end
 end
 
 Benchmark.benchmark(CAPTION, 10, FORMAT) do |x|
-  x.report("Ruby 10th fib: ") { Fib.ruby_fib(10) }
-  x.report("Rust 10th fib: ") { Fib.rust_fib(10) }
+  puts "Slow Fibs"
+  x.report("Ruby 35th: ") { Fib.slow_ruby_fib(35) }
+  x.report("Rust 35th: ") { Fib.slow_rust_fib(35) }
   puts
-  x.report("Ruby 20th fib: ") { Fib.ruby_fib(20) }
-  x.report("Rust 20th fib: ") { Fib.rust_fib(20) }
+  x.report("Ruby 40th: ") { Fib.slow_ruby_fib(40) }
+  x.report("Rust 40th: ") { Fib.slow_rust_fib(40) }
   puts
-  x.report("Ruby 30th fib: ") { Fib.ruby_fib(30) }
-  x.report("Rust 30th fib: ") { Fib.rust_fib(30) }
+
+  puts "Fast Fibs"
+  x.report("Ruby 40th: ") { Fib.ruby_fib(40) }
+  x.report("Rust 40th: ") { Fib.rust_fib(40) }
   puts
-  x.report("Ruby 40th fib: ") { Fib.ruby_fib(40) }
-  x.report("Rust 40th fib: ") { Fib.rust_fib(40) }
+  x.report("Ruby 90th: ") { Fib.ruby_fib(90) }
+  x.report("Rust 90th: ") { Fib.rust_fib(90) }
 end
